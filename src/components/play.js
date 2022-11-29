513b6d7d3b6c40def2c157b7b0c69ef5b9fbd088
Jonathan Ung <53621118+jonathanung@users.noreply.github.com>
2022-11-28 22:14:29 -0800

    Update play.js

--- src/components/play.js
+++ src/components/play.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState}  from 'react';
+import { useState, useEffect }  from 'react';
 import "../App.css";
 import DisplayCard from './displayCard';
 import LastPair from './LastPair';
