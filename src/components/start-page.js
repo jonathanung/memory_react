2341f715e3d5c770fdbb5cdd404823bc78d41c8d
Jonathan Ung <53621118+jonathanung@users.noreply.github.com>
2022-11-28 23:06:42 -0800

    Update start-page.js

--- src/components/start-page.js
+++ src/components/start-page.js
@@ -26,12 +26,14 @@ const StartPage = (props) => {
           <button type="button" onClick={startGame} >Start</button>
           <button type="button" id="leaderboard-btn" onClick={() => swapMode()}>Leaderboard</button>
           <button type='button' onClick={() => alert("I love sam :)")}>Nathan's Super Special Button</button>
-          <p>Memory is the card game where you try to flip 2 of the same card! Each correct pair gives you 2 extra seconds on the clock, but be careful since each wrong pair takes a second away! You also get time added on for each level passed!</p>
+          <p>Memory is the card game where you try to flip 2 of the same card! Each correct pair gives you 2 extra seconds on the clock, but be careful since each wrong pair takes 2 seconds away! You also get time added on for each level passed!</p>
         </div>
     )
   } else {
     return (<Leaderboard isMenu={true} swapMode={swapMode} />)
   }
 }
+
+export default StartPage;
 
 export default StartPage;
