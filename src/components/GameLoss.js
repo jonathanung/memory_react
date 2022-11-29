c1a13030acb34f4843e439d884d3862d61b65980
Jonathan Ung <53621118+jonathanung@users.noreply.github.com>
2022-11-28 23:04:24 -0800

    Update GameLoss.js

--- src/components/GameLoss.js
+++ src/components/GameLoss.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import LeaderboardForm from './leaderboard-form';
+import Leaderboard from './leaderboard';
+import { Container } from 'reactstrap';
 
 export default function GameLoss(props) {
     const reload = () => {
         window.location.reload(false)
     }
     return (
-        <div className="game-loss">
-            <h1>Your score: {props.score}</h1> 
-            <h4>You managed to get to level {props.level}!</h4>
-            <LeaderboardForm score={props.score} />
-        </div>
+        <Container className="game-menu">
+            <div className="game-loss">
+                <h1>Your score: {props.score}</h1> 
+                <h4>You managed to get to level {props.level}!</h4>
+                <LeaderboardForm score={props.score} />
+            </div>
+            <Leaderboard/>
+        </Container>
     );
 }
